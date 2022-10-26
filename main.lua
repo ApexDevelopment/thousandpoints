1468bb3ec38c8279b11a7ec514f59495711b5750
Micah Havens <micah.apex@gmail.com>
2022-10-26 00:03:12 -0400

    Allow loading of modules by name

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ local game = {
 	points = 0,
 	current_module = nil,
 	switch_module = function(self, new_module)
+		if type(new_module) == "string" then new_module = modules[new_module] end
+		
 		if self.current_module and self.current_module.stop then
 			self.current_module.stop()
 		end
