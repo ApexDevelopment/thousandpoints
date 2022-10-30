952bec3c3f8e554668c1c3ffde538c6450419d6d
Micah Havens <micah.apex@gmail.com>
2022-10-30 18:01:40 -0400

    Make functions local

--- modules/testmodule.lua
+++ modules/testmodule.lua
@@ -1,19 +1,19 @@
-function start()
+local function start()
 	print("Test module started.")
 end
 
-function stop()
+local function stop()
 	print("Test module stopped.")
 end
 
-function update(dt, game)
+local function update(dt, game)
 end
 
-function draw(game)
+local function draw(game)
 	love.graphics.print("This is a test module. Press 'b' to go back to the main menu.", 10, 50)
 end
 
-function keypressed(key, game)
+local function keypressed(key, game)
 	if key == "b" then
 		game:switch_module("mainmenu")
 	end
