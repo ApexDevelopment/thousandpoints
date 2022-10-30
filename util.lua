8c66c984feead301bc6fb034456136625f035a35
Micah Havens <micah.apex@gmail.com>
2022-10-30 17:56:43 -0400

    Make util functions local

--- util.lua
+++ util.lua
@@ -1,12 +1,12 @@
-function point_in_rect(px, py, rx, ry, rw, rh)
+local function point_in_rect(px, py, rx, ry, rw, rh)
 	return px > rx and py > ry and px < rx + rw and py < ry + rh
 end
 
-function center_h(drawable, screen_w)
+local function center_h(drawable, screen_w)
 	return screen_w / 2 - drawable:getWidth() / 2
 end
 
-function center_v(drawable, screen_h)
+local function center_v(drawable, screen_h)
 	return screen_h / 2 - drawable:getHeight() / 2
 end
 
