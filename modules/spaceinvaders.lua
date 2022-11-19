8e3aaad46f63103a45d3555a026899be9a6669d2
Micah Havens <micah.apex@gmail.com>
2022-11-19 17:06:23 -0500

    Make space invaders much more functional

--- modules/spaceinvaders.lua
+++ modules/spaceinvaders.lua
@@ -1,7 +1,7 @@
 local FIELD_WIDTH = 60
 local FIELD_HEIGHT = 40
 local TICKS_PER_SECOND = 20
-local NUM_ALIENS_IN_ROW = (FIELD_WIDTH - 2)
+local NUM_ALIENS_IN_ROW = (FIELD_WIDTH - 4)
 local NUM_ALIEN_ROWS = 4
 local NUM_ALIENS = NUM_ALIENS_IN_ROW * NUM_ALIEN_ROWS
 
@@ -14,14 +14,14 @@ local alien_direction = 1
 local alien_tick = 0
 local alien_tick_threshold = TICKS_PER_SECOND * 5
 
-local bullet_position = nil
+local bullet_positions = {}
 local bullet_velocity = 1
 
 local score = 0
 
 local function reset_alien_positions()
 	for i = 1, NUM_ALIENS do
-		alien_positions[i] = {x = (i - 1) % NUM_ALIENS_IN_ROW + 1, y = math.floor((i - 1) / NUM_ALIENS_IN_ROW) + 1}
+		alien_positions[i] = {x = (i - 1) % NUM_ALIENS_IN_ROW, y = math.floor((i - 1) / NUM_ALIENS_IN_ROW) + 1}
 	end
 	-- for i = 1, NUM_ALIENS do
 	-- 	alien_positions[i] = {x = i % NUM_ALIENS_IN_ROW, y = math.floor(i / NUM_ALIENS_IN_ROW)}
@@ -35,52 +35,62 @@ local function move_aliens_down()
 end
 
 local function update_alien_positions()
-	if alien_tick % 10 == 0 then
+	alien_tick = alien_tick + 1
+
+	if alien_tick % 20 == 0 then
+		-- Check for collisions with the left and right of the field.
+		for i = 1, #alien_positions do
+			if alien_positions[i].x == 0 and alien_direction == -1 then
+				alien_direction = -alien_direction
+				move_aliens_down()
+				return
+			elseif alien_positions[i].x == FIELD_WIDTH - 1 and alien_direction == 1 then
+				alien_direction = -alien_direction
+				move_aliens_down()
+				return
+			end
+		end
+
 		for i = 1, #alien_positions do
 			alien_positions[i].x = alien_positions[i].x + alien_direction
 		end
 	end
 
-	alien_tick = alien_tick + 1
-
+--[[
 	if alien_tick >= alien_tick_threshold then
 		alien_tick = 0
-		alien_direction = -alien_direction
+		--alien_direction = -alien_direction
 		move_aliens_down()
+	end]]
+end
+
+local function update_bullet()
+	if #bullet_positions == 0 then
+		return
 	end
 
-	for i = 1, #alien_positions do
-		if alien_positions[i].x == 0 or alien_positions[i].x == FIELD_WIDTH - 1 then
-			alien_direction = -alien_direction
-			--alien_tick_threshold = alien_tick_threshold - 5
-			return
+	for i = #bullet_positions, 1, -1 do
+		bullet_positions[i].y = bullet_positions[i].y - bullet_velocity
+
+		if bullet_positions[i].y < 0 then
+			table.remove(bullet_positions, i)
 		end
 	end
 end
 
-local function update_bullet()
-	if bullet_position == nil then
-		return
-	end
-
-	bullet_position.y = bullet_position.y - bullet_velocity
-
-	if bullet_position.y < 0 then
-		bullet_position = nil
-	end
-end
-
 local function check_bullet_collision()
-	if bullet_position == nil then
+	if #bullet_positions == 0 then
 		return
 	end
 
-	for i = 1, #alien_positions do
-		if alien_positions[i].x == bullet_position.x and alien_positions[i].y == bullet_position.y then
-			table.remove(alien_positions, i)
-			bullet_position = nil
-			score = score + 1
-			return
+	for i = 1, #bullet_positions do
+		for j = 1, #alien_positions do
+			if bullet_positions[i].x == alien_positions[j].x and bullet_positions[i].y == alien_positions[j].y then
+				table.remove(bullet_positions, i)
+				table.remove(alien_positions, j)
+				score = score + 100
+				return
+			end
 		end
 	end
 end
@@ -104,8 +114,8 @@ local function update_player_position()
 end
 
 local function fire_bullet()
-	if bullet_position == nil and love.keyboard.isDown("space") then
-		bullet_position = { x = ship_position, y = FIELD_HEIGHT - 2 }
+	if #bullet_positions < 2 and love.keyboard.isDown("space") then
+		table.insert(bullet_positions, {x = ship_position, y = FIELD_HEIGHT - 2})
 	end
 end
 
@@ -127,23 +137,28 @@ end
 
 local function draw(game)
 	local PIXEL_SIZE = game.settings.PIXEL_SIZE
+
+	love.graphics.push()
+	love.graphics.translate(love.graphics.getWidth() / 2 - FIELD_WIDTH * PIXEL_SIZE / 2, love.graphics.getHeight() / 2 - FIELD_HEIGHT * PIXEL_SIZE / 2)
+
 	love.graphics.setColor(1, 1, 1)
 	love.graphics.rectangle("line", 0, 0, FIELD_WIDTH * PIXEL_SIZE, FIELD_HEIGHT * PIXEL_SIZE)
 
-	love.graphics.push()
 	love.graphics.scale(PIXEL_SIZE, PIXEL_SIZE)
 
+	-- Draw aliens
 	love.graphics.setColor(1, 1, 1)
 	for i = 1, #alien_positions do
 		love.graphics.rectangle(
 			"fill",
-			alien_positions[i].x,
-			alien_positions[i].y,
-			1,
-			1
+			alien_positions[i].x + 0.1,
+			alien_positions[i].y + 0.1,
+			0.8,
+			0.8
 		)
 	end
 
+	-- Draw ship
 	love.graphics.setColor(1, 0, 0)
 	love.graphics.rectangle(
 		"fill",
@@ -153,15 +168,19 @@ local function draw(game)
 		1
 	)
 
-	if bullet_position ~= nil then
+	-- Draw bullet
+	if #bullet_positions ~= 0 then
 		love.graphics.setColor(0, 1, 0)
-		love.graphics.rectangle(
-			"fill",
-			bullet_position.x,
-			bullet_position.y,
-			1,
-			1
-		)
+
+		for i = 1, #bullet_positions do
+			love.graphics.rectangle(
+				"fill",
+				bullet_positions[i].x + 0.2,
+				bullet_positions[i].y,
+				0.6,
+				1
+			)
+		end
 	end
 
 	love.graphics.pop()
